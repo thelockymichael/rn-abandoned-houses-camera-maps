7cf100a874cf0452233b5b7e599e8be3b964d906
Michael Lock <thelockymichael@gmail.com>
2021-01-07 23:49:41 +0200

    added screens

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const NewPlaceScreen = props => {
+  return (
+    <View>
+      <Text>NewPlaceScreen</Text>
+    </View>
+  )
+}
+NewPlaceScreen.navigationOptions = {
+  headerTitle: "New Place"
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default NewPlaceScreen
