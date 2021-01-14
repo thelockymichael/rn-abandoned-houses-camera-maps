43ad669b8eecd8ade411f594801f2470922189a4
Michael Lock <thelockymichael@gmail.com>
2021-01-14 17:07:46 +0200

    list places

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -2,9 +2,17 @@ import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
 const PlaceDetailScreen = props => {
-  return <View>
-    <Text>PlaceDetailScreen</Text>
-  </View>
+  return (
+    <View>
+      <Text>PlaceDetailScreen</Text>
+    </View>
+  )
+}
+
+PlaceDetailScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('placeTitle')
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -4,7 +4,7 @@ import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 import {useSelector} from "react-redux"
 
 import HeaderButton from "../components/HeaderButton"
-
+import PlaceItem from "../components/PlaceItem"
 
 const PlacesListScreen = props => {
 
@@ -14,7 +14,20 @@ const PlacesListScreen = props => {
     <FlatList
       data={places}
       keyExtractor={item => item.id}
-      renderItem={itemData => {}}
+      renderItem={itemData => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate('PlaceDetail', {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id
+            })
+          }}
+        />
+      )
+      }
     />
   )
 }
