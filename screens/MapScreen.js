8bfc6f6b7252caa365430be754582c846bc00a0c
Michael Lock <thelockymichael@gmail.com>
2021-01-16 20:42:47 +0200

    making the picked location saveable

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,23 +1,111 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
-import MapView from "react-native-maps"
+import React,
+{
+  useState,
+  useEffect,
+  useCallback
+} from 'react'
+
+import {
+  Text,
+  View,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from 'react-native'
+import MapView, {Marker} from "react-native-maps"
+import Colors from '../constants/Colors'
 
 const MapScreen = props => {
+  const [selectedLocation, setSelectedLocation] = useState()
+
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421
   }
+
+  const selectLocationHandler = event => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude
+    })
+  }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      // could show an alert!
+      Alert.alert(
+        'Pick a location.',
+        'You did not pick a location!',
+        [{text: 'Okay'}]
+      )
+      return
+    }
+    props.navigation.navigate('NewPlace',
+      {pickedLocation: selectedLocation})
+  }, [selectedLocation])
+
+  useEffect(() => {
+    props.navigation.setParams({
+      saveLocation: savePickedLocationHandler
+    })
+  }, [savePickedLocationHandler])
+
+  let markerCoordinates
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng
+    }
+  }
+
   return <MapView
     style={styles.map}
     region={mapRegion}
-  />
+    onPress={selectLocationHandler}
+  >
+    {markerCoordinates &&
+      <Marker
+        title="Picked Location"
+        coordinate={markerCoordinates}
+      >
+      </Marker>
+    }
+  </MapView>
 }
+
+MapScreen.navigationOptions = navData => {
+  const saveFn = navData.navigation.getParam('saveLocation')
+
+  return {
+    headerRight: () =>
+      <TouchableOpacity
+        style={styles.headerButton}
+        onPress={saveFn}
+      >
+        <Text
+          style={styles.headerButtonText}
+        >
+          Save
+        </Text>
+      </TouchableOpacity>
+  }
+}
+
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+  headerButton: {
+    marginHorizontal: 20,
+  },
+  headerButtonText: {
+    fontSize: 16,
+    color: Platform.OS === 'android' ? 'white' : Colors.primary
   }
 })
 
